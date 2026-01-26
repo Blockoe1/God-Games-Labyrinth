fac53af00b76a1a754abf35c2d5b50494349e71d
Eli Koederitz <bkoederitz@mail.bradley.edu>
2026-01-26 11:31:13 -0600

    Player Moves

--- Assets/Scripts/Champions/ChampionMovement.cs
+++ Assets/Scripts/Champions/ChampionMovement.cs
@@ -21,9 +21,15 @@ namespace GGL.Champions
         private const string MOVE_ACTION_NAME = "Move";
         #endregion
 
-        private Vector2 moveInput;
+        [SerializeField] private float maxSpeed;
+        [SerializeField] private float acceleration;
+        [SerializeField] private bool positionSnap;
 
         private InputAction moveAction;
+
+        private float speed;
+        private Vector2 moveInput;
+        private bool markForSnap;
        
         #region Component References
         [Header("Components")]
@@ -59,15 +65,49 @@ namespace GGL.Champions
         }
 
         #region Input Functions
+        /// <summary>
+        /// Read player movement input.
+        /// </summary>
+        /// <param name="obj"></param>
         private void MoveAction_performed(InputAction.CallbackContext obj)
         {
-            throw new System.NotImplementedException();
-        }
+            // Only take the X or Y Component for locked movement.
+            Vector2 rawInput = obj.ReadValue<Vector2>().normalized;
+            Vector2 oldMoveInput = moveInput;
+            moveInput = Mathf.Abs(rawInput.y) > Mathf.Abs(rawInput.x) ? Vector2.up * System.MathF.Sign(rawInput.y) :
+                Vector2.right * System.MathF.Sign(rawInput.x);
 
+            //Snap the player's position to the grid when they change direction.
+            if (positionSnap && moveInput != oldMoveInput)
+            {
+                markForSnap = true;
+            }
+
+        }
         private void MoveAction_canceled(InputAction.CallbackContext obj)
         {
-            throw new System.NotImplementedException();
+            moveInput = Vector2.zero;
         }
         #endregion
+
+        /// <summary>
+        /// Control movement in FixedUpdate
+        /// </summary>
+        /// <remarks>
+        /// Using FixedUpdate instead of a corutine because FixedUpdate happens before internal physics updates, while
+        /// WaitForFixedUpdate happens after.
+        /// </remarks>
+        private void FixedUpdate()
+        {
+            speed = Mathf.MoveTowards(speed, maxSpeed, acceleration * Time.fixedDeltaTime);
+            rb.linearVelocity = speed * moveInput;
+
+            // Snap the player's position tot he grid when they change direction.
+            if (markForSnap)
+            {
+                rb.MovePosition(MathHelpers.RoundVectorToInt(rb.position));
+                markForSnap = false;
+            }
+        }
     }
 }

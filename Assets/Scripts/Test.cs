a6164f84047948e109037cf74ecf0feceb6a5665
Eli Koederitz <bkoederitz@mail.bradley.edu>
2026-01-26 10:23:05 -0600

    Update Test.cs

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -7,7 +7,7 @@ namespace GGL
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         void Start()
         {
-        
+            Debug.Log("Something");
         }
 
         // Update is called once per frame

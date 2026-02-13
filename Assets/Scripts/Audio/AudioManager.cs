c83a6a981290713bae346b82fe9a9133cc400c09
Juke422B <jherron@mail.bradley.edu>
2026-02-12 20:58:20 -0600

    audio manager should be complete

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -6,11 +6,24 @@ namespace GGL
 {
     public class AudioManager : MonoBehaviour
     {
+        private EventInstance musicEventInstance;
+
         public static AudioManager instance { get; private set; }
 
         private void Awake()
         {
             instance = this;
+        }
+
+        private void Start()
+        {
+            InitializeMusic(FmodEvents.instance.music);
+        }
+
+        private void InitializeMusic(EventReference musicEventReference)
+        {
+            musicEventInstance = CreateInstance(musicEventReference);
+            musicEventInstance.start();
         }
 
         public void PlayOneShot(EventReference sound, Vector3 worldPos)

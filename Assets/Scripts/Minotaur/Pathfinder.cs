4d3f23705885369132b170aae53196c7ecad4230
Eli Koederitz <bkoederitz@mail.bradley.edu>
2026-02-15 14:01:25 -0600

    Working on Pathfinding

--- Assets/Scripts/Minotaur/Pathfinder.cs
+++ Assets/Scripts/Minotaur/Pathfinder.cs
@@ -6,6 +6,8 @@
 //
 // Brief Description : Finds a path along the grid from one space to another using JPA.
 *****************************************************************************/
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
@@ -13,6 +15,16 @@ namespace GGL.Minotaur
 {
     public class Pathfinder : MonoBehaviour
     {
+        #region CONSTS
+        private static readonly Vector2Int[] DEFAULT_NODE_SUCCESSORS = new Vector2Int[]
+        {
+            Vector2Int.right,
+            Vector2Int.left,
+            Vector2Int.up,
+            Vector2Int.down,
+        };
+        #endregion
+
         [SerializeField, Tooltip("The tilemap to use as wall based tiles.")] private Tilemap collisionTilemap;
         [SerializeField] private Transform debugPosition;
 
@@ -23,8 +35,11 @@ namespace GGL.Minotaur
         private class PathNode
         {
             internal Vector2Int tile;
+            internal Vector2Int[] successors;
             internal int g;
             internal int h;
+
+            internal PathNode previousNode;
 
             internal int F
             {
@@ -34,9 +49,11 @@ namespace GGL.Minotaur
                 }
             }
 
-            internal PathNode(Vector2Int tile)
+            internal PathNode(Vector2Int tile, Vector2Int[] successors, Vector2Int start, Vector2Int end)
             {
                 this.tile = tile;
+                this.successors = successors;
+                CalculateMDistance(start, end); 
             }
 
             /// <summary>
@@ -68,8 +85,69 @@ namespace GGL.Minotaur
         /// <returns>The sequence of nodes from the starting to the ending tiles.</returns>
         public Vector2[] FindPath(Vector2Int startingTile, Vector2Int endingTile)
         {
-            throw new System.NotImplementedException();
+            // Finalizes the path by looping through nodes in reverse order and adding their position to a list.
+            Vector2[] FinalizePath(PathNode startNode, PathNode lastNode)
+            {
+                List<Vector2> result = new List<Vector2>();
+                PathNode current = lastNode;
+                while (current != startNode)
+                {
+                    result.Add(TileToPos(current.tile));
+                    current = current.previousNode;
+                }
+                result.Reverse();
+                return result.ToArray();
+            }
+
+            List<PathNode> openList = new List<PathNode>();
+            List<PathNode> closedList = new List<PathNode>();
+
+            // Create the first node in the path that has a 0 direction, so all directions are searched.
+            PathNode startNode = new PathNode(startingTile, DEFAULT_NODE_SUCCESSORS, startingTile, endingTile);
+            openList.Add(startNode);
+
+            while (openList.Count > 0)
+            {
+                // Get the node that has the lowest cost to evaluate next.
+                PathNode currentNode = openList.OrderBy(item => item.F).FirstOrDefault();
+                // Mark the current node as closed.
+                openList.Remove(currentNode);
+                closedList.Add(currentNode);
+
+                // If we reached the ending node, then finish the pathfinding and construct our path.
+                if (currentNode.tile == endingTile)
+                {
+                    // Construct path.
+                    return FinalizePath(startNode, currentNode);
+                }
+
+                // Functions for checking forced neighbors along the horizontal and vertical directions.  Vertical
+                // is the dominant direction, so it has to check for horizontals as well.
+                bool CheckHorizontal(Vector2Int startingTile, Vector2Int direction)
+                {
+
+                }
+                bool CheckVertical(Vector2Int startingTile, Vector2Int direction) // Dominant direction check.
+                {
+
+                }
+
+                // Perform checks for forced neighbors.
+                foreach(Vector2Int successorDirection in currentNode.successors)
+                {
+                    // If the successor direction is vertical, perform additional checks.
+                    if (successorDirection.y > 0)
+                    {
+
+                    }
+                }
+            }
+
+            // If we cannot find a path, return null.
+            return null;
         }
+
+
 
         /// <summary>
         /// Gets the tile position of a certain world position.
@@ -79,6 +157,16 @@ namespace GGL.Minotaur
         private Vector2Int PosToTile(Vector2 pos)
         {
             return (Vector2Int)collisionTilemap.WorldToCell(pos);  
+        }
+
+        /// <summary>
+        /// Converts a tile position to a position in world space.
+        /// </summary>
+        /// <param name="tile">The tile to convert.</param>
+        /// <returns>The position of the tile in world space.</returns>
+        private Vector2 TileToPos(Vector2Int tile)
+        {
+            return collisionTilemap.
         }
 
         #region Debug

c236324c265c56feb9414899e0aa7e4d73c035eb
Eli Koederitz <bkoederitz@mail.bradley.edu>
2026-02-18 12:09:01 -0600

    Hitbox Setup

---
+++ Assets/Scripts/Minotaur/StateMachine/MinotaurHitbox.cs
@@ -0,0 +1,17 @@
+/*****************************************************************************
+// File Name : HitboxRelay.cs
+// Author : Brandon Koederitz
+// Creation Date : 2/18/2026
+// Last Modified : 2/18/2026
+//
+// Brief Description : Relays collision information to a parent MinotaurAttacker Component.
+*****************************************************************************/
+using UnityEngine;
+
+namespace GGL.Minotaur
+{
+    public class MinotaurHitbox : MonoBehaviour
+    {
+        
+    }
+}

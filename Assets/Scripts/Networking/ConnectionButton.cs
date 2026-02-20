ade89fa7f1e52ca635e8ac4eff81fa06b2619379
Eli Koederitz <bkoederitz@mail.bradley.edu>
2026-02-20 16:10:24 -0600

    Connection Button Working

--- Assets/Scripts/Networking/ConnectionButton.cs
+++ Assets/Scripts/Networking/ConnectionButton.cs
@@ -22,6 +22,7 @@ namespace GGL.Networking
         /// </summary>
         private void Awake()
         {
+            connectAction.Enable();
             connectAction.performed += ConnectAction_performed;
         }
         private void OnDestroy()
@@ -35,15 +36,22 @@ namespace GGL.Networking
         /// <param name="obj"></param>
         private void ConnectAction_performed(InputAction.CallbackContext obj)
         {
+            if (NetworkManager.Singleton == null) { return; }
             if (isHost)
             {
-                Debug.Log("Connecting as host.");
-                NetworkManager.Singleton.StartHost();
+                if (!NetworkManager.Singleton.IsHost)
+                {
+                    Debug.Log("Connecting as host.");
+                    NetworkManager.Singleton.StartHost();
+                }
             }
             else
             {
-                Debug.Log("Connecting as client.");
-                NetworkManager.Singleton.StartClient();
+                if (!NetworkManager.Singleton.IsClient)
+                {
+                    Debug.Log("Connecting as client.");
+                    NetworkManager.Singleton.StartClient();
+                }
             }
         }
     }

9bfa37e119b8812221b2ba21cd7967de2bffd18d
Juke422B <jherron@mail.bradley.edu>
2026-02-22 12:07:44 -0600

    readyupsystem

---
+++ Assets/Scripts/UI/ReadyUpScripts/ReadyUpHandler.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace GGL
+{
+    public class ReadyUpHandler : MonoBehaviour
+    {
+        [SerializeField] private GameObject Player1;
+        [SerializeField] private GameObject Player2;
+        [SerializeField] private GameObject Player3;
+        [SerializeField] private GameObject Player4;
+
+        [SerializeField] private TMP_Text timerText;
+
+        private bool timerRunning = false;
+
+        private void Update()
+        {
+            if (Player1.GetComponent<ReadyUpSystem1>().ready && Player2.GetComponent<ReadyUpSystem2>().ready && 
+                Player3.GetComponent<ReadyUpSystem3>().ready && Player4.GetComponent<ReadyUpSystem4>().ready && !timerRunning)
+            {
+                StartCoroutine(StartGame());
+            }
+        }
+        IEnumerator StartGame()
+        {
+            timerRunning = true;
+            int i = 3;
+            while (i > 0)
+            {
+                if (!Player1.GetComponent<ReadyUpSystem1>().ready || !Player2.GetComponent<ReadyUpSystem2>().ready || 
+                    !Player3.GetComponent<ReadyUpSystem3>().ready || !Player4.GetComponent<ReadyUpSystem4>().ready)
+                {
+                    break;
+                }
+                timerText.text = "" + i;
+                yield return new WaitForSecondsRealtime(1f);
+                i--;
+            }
+            if (!Player1.GetComponent<ReadyUpSystem1>().ready || !Player2.GetComponent<ReadyUpSystem2>().ready ||
+                    !Player3.GetComponent<ReadyUpSystem3>().ready || !Player4.GetComponent<ReadyUpSystem4>().ready)
+            {
+                StopCoroutine(StartGame());
+                timerRunning = false;
+                timerText.text = "";
+            }
+            else if (Player1.GetComponent<ReadyUpSystem1>().ready && Player2.GetComponent<ReadyUpSystem2>().ready &&
+                Player3.GetComponent<ReadyUpSystem3>().ready && Player4.GetComponent<ReadyUpSystem4>().ready)
+            {
+                SceneManager.LoadScene("MazeScene");
+            }
+        }
+    }
+}

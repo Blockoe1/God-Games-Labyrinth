c17ffa3999c24c1a26dac7cc64608a10a8c66367
Juke422B <jherron@mail.bradley.edu>
2026-02-27 14:25:08 -0600

    Uses Unity Events for Minotaur now

--- Assets/Scripts/UI/TimerScript.cs
+++ Assets/Scripts/UI/TimerScript.cs
@@ -13,7 +13,8 @@ namespace GGL
         [SerializeField] float time;
         [SerializeField] TMP_Text timerText;
         [SerializeField] UnityEvent OnTimerComplete;
-        [SerializeField] GameObject Minotaur;
+        [SerializeField] UnityEvent MinotaurSpawn;
+        [SerializeField] UnityEvent MinotaurEnrage;
         [SerializeField] Slider Timer1;
         [SerializeField] Slider Timer2;
         [SerializeField] Slider Timer3;
@@ -46,7 +47,7 @@ namespace GGL
             }
 
             //30 seconds have passed, spawn Minotaur
-            Minotaur.SetActive(true);
+            MinotaurSpawn?.Invoke();
             minotaurImage.gameObject.SetActive(false);
 
             while (time > 45)
@@ -63,8 +64,10 @@ namespace GGL
                 timerText.text = "" + time;
                 Timer3.value = ((time - 15) / 30);
             }
+
             //60 seconds have passed, 30 left
-            //enrage minotaur i guess
+            MinotaurEnrage?.Invoke();
+
             while (time > 15)
             {
                 yield return null;

d4c26576266ea0267b78727367f03e8b4b5ee8da
Juke422B <jherron@mail.bradley.edu>
2026-03-06 15:05:25 -0600

    i think it should work but i dont think it works without the network stuff working

--- Assets/Scripts/UI/WinSceneScript.cs
+++ Assets/Scripts/UI/WinSceneScript.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using System.Collections;
 using TMPro;
 using UnityEngine;
@@ -8,6 +9,21 @@ namespace GGL
     public class WinSceneScript : MonoBehaviour
     {
         [SerializeField] private TMP_Text timerText;
+        
+        private bool zeusWin = false;
+        private bool poseidonWin = false;
+        private bool athenaWin = false;
+        private bool aphroditeWin = false;
+
+        [SerializeField] GameObject zeusWinName;
+        [SerializeField] GameObject zeusWinQuote;
+        [SerializeField] GameObject poseidonWinName;
+        [SerializeField] GameObject poseidonWinQuote;
+        [SerializeField] GameObject athenaWinName;
+        [SerializeField] GameObject athenaWinQuote;
+        [SerializeField] GameObject aphroditeWinName;
+        [SerializeField] GameObject aphroditeWinQuote;
+
         private void Start()
         {
             StartCoroutine(WinScreenTimer());
@@ -21,8 +37,30 @@ namespace GGL
         IEnumerator WinScreenTimer()
         {
             yield return new WaitForSecondsRealtime(1f);
-            //check which god won
-            //enable their UI
+            while (!zeusWin && !poseidonWin && !athenaWin && !aphroditeWin)
+            {
+                yield return null;
+            }
+            if (zeusWin)
+            {
+                zeusWinName.SetActive(true);
+                zeusWinQuote.SetActive(true);
+            }
+            else if (poseidonWin)
+            {
+                poseidonWinName.SetActive(true);
+                poseidonWinQuote.SetActive(true);
+            }
+            else if (athenaWin)
+            {
+                athenaWinName.SetActive(true);
+                athenaWinQuote.SetActive(true);
+            }
+            else if (aphroditeWin)
+            {
+                aphroditeWinName.SetActive(true);
+                aphroditeWinQuote.SetActive(true);
+            }
             int i = 5;
             while (i > 0)
             {
@@ -31,6 +69,27 @@ namespace GGL
                 i--;
             }
             SceneManager.LoadScene("StartScene");
+        }
+
+        public void RecieveScores(int[] scores)
+        {
+            //zeus, poseidon, athena, aphrodite
+            if (scores[0] > scores[1] && scores[0] > scores[2] && scores[0] > scores[3])
+            {
+                zeusWin = true;
+            }
+            else if (scores[1] > scores[0] && scores[1] > scores[2] && scores[1] > scores[3])
+            {
+                poseidonWin = true;
+            }
+            else if (scores[2] > scores[0] && scores[2] > scores[1] && scores[2] > scores[3])
+            {
+                athenaWin = true;
+            }
+            else if (scores[3] > scores[0] && scores[3] > scores[1] && scores[3] > scores[2])
+            {
+                aphroditeWin = true;
+            }
         }
     }
 }

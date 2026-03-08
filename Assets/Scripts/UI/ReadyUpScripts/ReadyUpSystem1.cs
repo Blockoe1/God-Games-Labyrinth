55e61ef1b714d3500a79f5e5b3770fb2db34d1e2
Juke422B <jherron@mail.bradley.edu>
2026-03-08 12:27:44 -0500

    updates to ui

--- Assets/Scripts/UI/ReadyUpScripts/ReadyUpSystem1.cs
+++ Assets/Scripts/UI/ReadyUpScripts/ReadyUpSystem1.cs
@@ -23,5 +23,36 @@ namespace GGL
                 notReadyObject.SetActive(true);
             }
         }
+
+        void OnSteal()
+        {
+            if (!ready)
+            {
+                ready = true;
+                notReadyObject.SetActive(false);
+                readyObject.SetActive(true);
+            }
+            else if (ready)
+            {
+                ready = false;
+                readyObject.SetActive(false);
+                notReadyObject.SetActive(true);
+            }
+        }
+        void OnReady()
+        {
+            if (!ready)
+            {
+                ready = true;
+                notReadyObject.SetActive(false);
+                readyObject.SetActive(true);
+            }
+            else if (ready)
+            {
+                ready = false;
+                readyObject.SetActive(false);
+                notReadyObject.SetActive(true);
+            }
+        }
     }
 }

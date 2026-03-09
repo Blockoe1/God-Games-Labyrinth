6ce679ebf21d3ac66a34d83b6d1bf64c47a6ae32
Eli Koederitz <bkoederitz@mail.bradley.edu>
2026-03-08 21:11:41 -0500

    Minotaur Collects Coins

--- Assets/Scripts/Scoring/GameplayScoring/Collector.cs
+++ Assets/Scripts/Scoring/GameplayScoring/Collector.cs
@@ -21,6 +21,7 @@ namespace GGL.Scoring
         [SerializeField] private UnityEvent<int> onCollectEvent;
         [SerializeField] private UnityEvent<int> onDropEvent;
         [SerializeField] private UnityEvent<int> onDepositEvent;
+        [SerializeField] private CollectableCashZone autoDepositTarget; 
         private readonly Queue<Collectable> heldCollectables = new();
 
         public bool DisableCollection { get; set; }
@@ -72,18 +73,27 @@ namespace GGL.Scoring
                 collision.gameObject.TryGetComponent(out Collectable collectable) && collectable.IsCollectable)
             {
                 ForceCollect(collectable);
+                if (autoDepositTarget != null)
+                {
+                    DepositCollectables(autoDepositTarget);
+                }
             }
 
             // Handles cashing collectables at a GoldCashZone
             if (collision.gameObject.TryGetComponent(out CollectableCashZone cashZone) && cashZone.Team == id.Team 
                 && heldCollectables.Count > 0)
             {
-                cashZone.CashCollectables(heldCollectables);
-                heldCollectables.Clear();
-                onDepositEvent?.Invoke(heldCollectables.Count);
+                DepositCollectables(cashZone);
 
                 //OnDeposit?.Invoke(heldCollectables.Count);
             }
+        }
+
+        private void DepositCollectables(CollectableCashZone cashZone)
+        {
+            cashZone.CashCollectables(heldCollectables);
+            heldCollectables.Clear();
+            onDepositEvent?.Invoke(heldCollectables.Count);
         }
 
         /// <summary>

cd2f1ccb054f99355c61e5bc077fe6d3b1b42f63
Eli Koederitz <bkoederitz@mail.bradley.edu>
2026-03-13 15:05:56 -0500

    More work on charging

--- Assets/Scripts/Minotaur/StateMachine/ChargeState.cs
+++ Assets/Scripts/Minotaur/StateMachine/ChargeState.cs
@@ -7,6 +7,7 @@
 // Brief Description : State for the minotaur charging at a player down a hallway.
 *****************************************************************************/
 using System.Collections;
+using System.Linq;
 using UnityEngine;
 
 namespace GGL.Minotaur
@@ -20,6 +21,14 @@ namespace GGL.Minotaur
 
         private Vector2 chargeDirection;
 
+        private float snortTime;
+        public override void OnStateEnter()
+        {
+            base.OnStateEnter();
+            var main = snortParticles.FirstOrDefault().main;
+            snortTime = main.duration + main.startLifetime.constant;
+        }
+
         /// <summary>
         /// Controls the sequencing of the minotaur charging.
         /// </summary>
@@ -29,12 +38,31 @@ namespace GGL.Minotaur
             chargeDirection = minotaur.movement.Direction;
 
             // Play snort particles
+            foreach(var particle in snortParticles )
+            {
+                particle.Play();
+            }
+            yield return new WaitForSeconds(snortTime);
 
 
             // Set velocity for charge.
+            chargeParticles.Play();
+            minotaur.movement.enabled = false;
 
-            // Detect wall for crash.
-            throw new System.NotImplementedException();
+            while(true)
+            {
+                minotaur.movement.Rigidbody.linearVelocity = chargeDirection * chargeSpeed;
+
+                // Detect wall for crash.
+                RaycastHit2D ray = Physics2D.Raycast(minotaur.movement.Rigidbody.position, direction, maxWallCheckDistance,
+                        GGLHelpers.MoveCheckMask | GGLHelpers.MazeMask);
+                // If the raycast hit nothing, this is a valid direction.
+                if (!ray)
+                {
+                    
+                }
+            }
+
         }
     }
 }
